f74859b6d36f1d675fabff4bc31bf8cde706dac2
Ilya Sokolov <developer.sokolov@gmail.com>
2019-12-22 23:31:20 +0300

    Add base code for three scenes screen

--- app/src/main/java/org/redbyte/animatron/MainActivity.kt
+++ app/src/main/java/org/redbyte/animatron/MainActivity.kt
@@ -1,12 +1,18 @@
 package org.redbyte.animatron
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.redbyte.animatron.threescenes.ThreeScenesActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnThreeScenes.setOnClickListener {
+            startActivity(Intent(this, ThreeScenesActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/org/redbyte/animatron/threescenes/ThreeScenesActivity.kt
@@ -0,0 +1,13 @@
+package org.redbyte.animatron.threescenes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.redbyte.animatron.R
+
+class ThreeScenesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three_scenes)
+    }
+}
