6c09435d5561b7710018383ed2204969d2cfdad4
Ilya Sokolov <developer.sokolov@gmail.com>
2019-12-22 23:44:18 +0300

    simple animation semicircle

--- app/src/main/java/org/redbyte/animatron/threescenes/ThreeScenesActivity.kt
+++ app/src/main/java/org/redbyte/animatron/threescenes/ThreeScenesActivity.kt
@@ -1,7 +1,9 @@
 package org.redbyte.animatron.threescenes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_three_scenes.*
 import org.redbyte.animatron.R
 
 class ThreeScenesActivity : AppCompatActivity() {
@@ -9,5 +11,28 @@ class ThreeScenesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_three_scenes)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        setupRecyclerView()
+        btnNext.setOnClickListener {
+            animateToSecondScene()
+        }
+    }
+
+    private fun animateToSecondScene() {
+        activity_gopher_root_motion.setTransitionListener(object : PlainTransitionListener() {
+            override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+                // TODO: Red_byte 2019-12-22 refresh view
+            }
+        })
+        //Start animation semicircle
+        activity_gopher_root_motion.setTransition(R.id.start, R.id.end)
+        activity_gopher_root_motion.transitionToEnd()
+    }
+
+    private fun setupRecyclerView() {
+        // TODO: Red_byte 2019-12-22 release it 
     }
 }
