9fea69af03811823696fe0225ecda79376f41953
Ilya Sokolov <developer.sokolov@gmail.com>
2019-12-26 23:02:04 +0300

    add simple animation listener + backgroundThreeScenes color

---
+++ app/src/main/java/org/redbyte/animatron/threescenes/PlainAnimationListener.kt
@@ -0,0 +1,12 @@
+package org.redbyte.animatron.threescenes
+
+import android.view.animation.Animation
+
+open class PlainAnimationListener : Animation.AnimationListener {
+    override fun onAnimationRepeat(animation: Animation?) {}
+
+    override fun onAnimationStart(animation: Animation?) {}
+
+    override fun onAnimationEnd(animation: Animation?) {}
+
+}
