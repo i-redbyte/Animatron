9348a99d5b499ec7688d44c776545dee4e53da7f
Ilya Sokolov <developer.sokolov@gmail.com>
2019-12-26 23:12:08 +0300

    basic scene mask anim

--- app/src/main/java/org/redbyte/animatron/threescenes/ThreeScenesActivity.kt
+++ app/src/main/java/org/redbyte/animatron/threescenes/ThreeScenesActivity.kt
@@ -1,6 +1,9 @@
 package org.redbyte.animatron.threescenes
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_three_scenes.*
@@ -24,7 +27,8 @@ class ThreeScenesActivity : AppCompatActivity() {
     private fun animateToSecondScene() {
         activity_gopher_root_motion.setTransitionListener(object : PlainTransitionListener() {
             override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
-                // TODO: Red_byte 2019-12-22 refresh view
+                ivSceneCircleMask.alpha = 1f
+                animateViewAlpha(ivSceneCircleMask, 10, 0)
             }
         })
         //Start animation semicircle
@@ -35,4 +39,30 @@ class ThreeScenesActivity : AppCompatActivity() {
     private fun setupRecyclerView() {
         // TODO: Red_byte 2019-12-22 release it 
     }
+}
+
+private fun animateViewAlpha(
+    view: View,
+    duration: Long,
+    delay: Long = 0,
+    onAnimationStarted: () -> Unit = {},
+    onAnimationCompleted: () -> Unit = {}
+) {
+    val alphaAnimation = AlphaAnimation(0.0f, 1.0f).apply {
+        this.duration = duration
+        startOffset = delay
+        fillAfter = true
+
+        setAnimationListener(object : PlainAnimationListener() {
+            override fun onAnimationStart(animation: Animation?) {
+                onAnimationStarted.invoke()
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                onAnimationCompleted.invoke()
+            }
+        })
+    }
+
+    view.startAnimation(alphaAnimation)
 }
